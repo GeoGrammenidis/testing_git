7386243943d75ac11e83daadfc25088b3735306c
George Grammenidis <grammenidis@yahoo.gr>
2022-09-27 15:57:42 +0300

    initial commit

---
+++ first_lesson/hello_world.py
@@ -0,0 +1,11 @@
+from re import X
+
+y = 3
+x = 5
+print("Hello world")
+print(x)
+print(y)
+
+#edw exei isodo dedomenwn apo ton xristi!
+name = input('Dwse to onoma sou: ')
+print("To onoma sou einai: " + name) 
